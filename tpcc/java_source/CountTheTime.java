f282ce34d6badc8680e832b70186c7a8b890054c
Ian White <ian.white@heirloomcomputing.com>
2018-03-26 13:37:59 -0500

    k

--- tpcc/java_source/CountTheTime.java
+++ tpcc/java_source/CountTheTime.java
@@ -2,16 +2,16 @@
 
 public class CountTheTime implements AutoCloseable {
 	private long _start;
-	//private long _end;
-	private String _method;
+    private long _end;
+    private String _method;
 	public  CountTheTime (String method) {
 		this._start = System.currentTimeMillis();
 		this._method = method;
 	}
 	
 	public void close() {
-		//long end = System.currentTimeMillis();
-		//System.out.println(_method + ":" + (end-_start));
+		long end = System.currentTimeMillis();
+		System.out.println(_method + ":" + (end-_start));
 	}
 	
 }
