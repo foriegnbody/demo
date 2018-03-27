3d58241127381c1e32d01a167581422382b50958
Ian White <ian.white@heirloomcomputing.com>
2018-03-27 11:32:06 -0500

    break

--- tpcc/java_source/CountTheTime.java
+++ tpcc/java_source/CountTheTime.java
@@ -1,17 +1,20 @@
-
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
 
 public class CountTheTime implements AutoCloseable {
-	//private long _start;
-    //private long _end;
-    //private String _method;
+	private long _start;
+    private long _end;
+    private String _method;
 	public  CountTheTime (String method) {
-		//_start = System.currentTimeMillis();
-		//this._method = method;
+		_start = System.currentTimeMillis();
+		this._method = method;
 	}
 	
 	public void close() {
-		//_end = System.currentTimeMillis();
-		//System.out.println(_method + ":" + (_end-_start));
+		_end = System.currentTimeMillis();
+		System.out.println(_method + ":" + (_end-_start));
+		Set<URL> sites = new HashSet<URL>();  // Noncompliant
 	}
 	
 }
