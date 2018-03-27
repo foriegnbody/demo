f5d10452b9437ee78ad400d07a61db2de1973aa6
Ian White <ian.white@heirloomcomputing.com>
2018-03-27 11:09:17 -0500

    Remark out logger code

--- tpcc/java_source/CountTheTime.java
+++ tpcc/java_source/CountTheTime.java
@@ -1,17 +1,17 @@
 
 
 public class CountTheTime implements AutoCloseable {
-	private long _start;
-    private long _end;
-    private String _method;
+	//private long _start;
+    //private long _end;
+    //private String _method;
 	public  CountTheTime (String method) {
-		_start = System.currentTimeMillis();
-		this._method = method;
+		//_start = System.currentTimeMillis();
+		//this._method = method;
 	}
 	
 	public void close() {
-		_end = System.currentTimeMillis();
-		System.out.println(_method + ":" + (_end-_start));
+		//_end = System.currentTimeMillis();
+		//System.out.println(_method + ":" + (_end-_start));
 	}
 	
 }
