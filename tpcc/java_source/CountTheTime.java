b6bbd58e8f5a4a879da3cac8e33d3b9936877d39
tpccuser <ianw@Ians-MacBook-Pro.local>
2018-04-02 18:15:03 -0500

    remark out

--- tpcc/java_source/CountTheTime.java
+++ tpcc/java_source/CountTheTime.java
@@ -3,20 +3,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class CountTheTime implements AutoCloseable {
-	private long start;
-    private long end;
-    private String method;
+	//private long start;
+    //private long end;
+    //private String method;
 	public  CountTheTime (String method) {
-		start = System.currentTimeMillis();
-		this.method = method;
+		//start = System.currentTimeMillis();
+		//this.method = method;
     
 	}
 	
 	public void close() {
-		end = System.currentTimeMillis();
-		System.out.println(method + ":" + (end-start));
-		Set<URL> sites = new HashSet<URL>();  // Noncompliant
-		System.out.print(sites.toString());
+		//end = System.currentTimeMillis();
+		//System.out.println(method + ":" + (end-start));
+		//Set<URL> sites = new HashSet<URL>();  // Noncompliant
+		//System.out.print(sites.toString());
 		
 	}
 	
