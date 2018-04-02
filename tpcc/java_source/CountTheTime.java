7036dc0c54d04c5a02c5ad4e122a3b9e5a3745b2
tpccuser <ianw@Ians-MacBook-Pro.local>
2018-04-02 17:13:45 -0500

    change variables

--- tpcc/java_source/CountTheTime.java
+++ tpcc/java_source/CountTheTime.java
@@ -3,18 +3,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class CountTheTime implements AutoCloseable {
-	private long _start;
-    private long _end;
-    private String _method;
+	private long start;
+    private long end;
+    private String method;
 	public  CountTheTime (String method) {
-		_start = System.currentTimeMillis();
-		this._method = method;
+		start = System.currentTimeMillis();
+		this.method = method;
     
 	}
 	
 	public void close() {
-		_end = System.currentTimeMillis();
-		System.out.println(_method + ":" + (_end-_start));
+		end = System.currentTimeMillis();
+		System.out.println(method + ":" + (end-start));
 		Set<URL> sites = new HashSet<URL>();  // Noncompliant
 		System.out.print(sites.toString());
 		
