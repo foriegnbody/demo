7889c0d6536232fd23cc2f9b6ec68278292d900c
tpccuser <ianw@Ians-MacBook-Pro.local>
2018-04-02 17:07:45 -0500

    Enabled Java code because I'm super smart

--- tpcc/java_source/CountTheTime.java
+++ tpcc/java_source/CountTheTime.java
@@ -3,20 +3,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class CountTheTime implements AutoCloseable {
-	//private long _start;
-    //private long _end;
-    //private String _method;
+	private long _start;
+    private long _end;
+    private String _method;
 	public  CountTheTime (String method) {
-		//_start = System.currentTimeMillis();
-		//this._method = method;
+		_start = System.currentTimeMillis();
+		this._method = method;
     
 	}
 	
 	public void close() {
-		//_end = System.currentTimeMillis();
-		//System.out.println(_method + ":" + (_end-_start));
-		//Set<URL> sites = new HashSet<URL>();  // Noncompliant
-		//System.out.print(sites.toString());
+		_end = System.currentTimeMillis();
+		System.out.println(_method + ":" + (_end-_start));
+		Set<URL> sites = new HashSet<URL>();  // Noncompliant
+		System.out.print(sites.toString());
 		
 	}
 	
